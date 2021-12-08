c3003e5695d964508c43e07a68112a18a31801b0
shash-2468 <shashanksuroju23@gmail.com>
2021-12-08 17:11:21 +0530

    made a yinyang design

---
+++ Yinyang.py
@@ -0,0 +1,44 @@
+import turtle
+
+yy = turtle.Turtle()
+yy.speed(speed='fastest')
+
+window = turtle.Screen()
+window.title("YinYang")
+window.bgcolor("#ff704d")
+
+def yinyang(radius, color1, color2):
+    yy.width(3)
+    yy.color(color1, color1)
+    yy.begin_fill()
+    yy.circle(radius/2., 180)
+    yy.circle(radius, 180)
+    yy.left(180)
+    yy.circle(-radius/2., 180)
+    yy.end_fill()
+    yy.left(90)
+    yy.up()
+    yy.forward(radius*0.35)
+    yy.right(90)
+    yy.down()
+    yy.color(color2, color2)
+    yy.begin_fill()
+    yy.circle(radius*0.15)
+    yy.end_fill()
+    yy.left(90)
+    yy.up()
+    yy.backward(radius*0.35)
+    yy.down()
+    yy.left(90)
+
+def main():
+    yy.reset()
+    yinyang(200, "black", "white")
+    yinyang(200, "white", "black")
+    yy.st()
+    return "yinyang"
+
+if __name__ == '__main__':
+    main()
+turtle.done()
+
