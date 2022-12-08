57a062eed7ccd4b80b77824975c6a16dc39f4577
Tushar Banik <107763774+Tushar98644@users.noreply.github.com>
2022-12-08 19:12:45 +0530

    Update superman logo.py

--- superman logo.py
+++ superman logo.py
@@ -1,195 +1,112 @@
 import turtle
-bob=turtle.Turtle()
-from Stuff import *
-bob.speed(0)
-turtle.bgcolor("black")
 
+t=turtle.Turtle()
+turtle.Screen().bgcolor('#f3f3f3')
 
+def curve(value): #create a function to generate curves in turtle
+    for i in range(value): #for loop to repeat the inputted value number of times
+        t.right(1) #step by step curve
+        t.forward(1)
 
-#extra blue point
-jump(bob,0,-300)
-box(bob,25,"blue",25)
-jump(bob,-25,-275)
-box(bob,75,"blue",25)
-jump(bob,-125,75)
-box(bob,275,"blue",25)
-jump(bob,-100,100)
-box(bob,225,"blue",25)
+t.penup() #pen is in the up position so it will not draw
+t.setposition(0,43) #move the pen to these x and y coordinates
+t.pendown() #pen is in the down position so it will draw
+t.begin_fill() #start filling in the shape
+t.pencolor('black') #change the pen color to black
+t.fillcolor('red') #change the shape fill color to maroon to match the Superman logo
+t.pensize(3) #use a pen size of 3 to create a black border
+t.forward(81.5) #the pen will move forward this number to start the shield of the logo
+t.right(49.4) #rotate the pen right 49.4 degrees
+t.forward(58) #move the pen forward by 58
+t.right(81.42) #rotate right by 81.42 degrees
+t.forward(182) #move the pen forward by 182
+t.right(98.36) #rotate the pen right by 98.36 degrees
+t.forward(182) #move the pen forward by 182
+t.right(81.42) #rotate the pen by 81.42 degrees to the right
+t.forward(58) #move the pen forward 58
+t.right(49.4) #rotate the pen to the right by 49.4
+t.forward(81.5) #move the pen forward by 81.5 to meet the start at the top of the shield
+t.end_fill() #complete the fill of the shield so the shape is closed off
+t.penup() #pen will not draw
 
-#right part
-jump(bob,25,-250)
-box(bob,50,"blue",25)
-jump(bob,50,-225)
-box(bob,50,"blue",25)
-jump(bob,75,-200)
-box(bob,50,"blue",25)
-jump(bob,100,-175)
-box(bob,50,"blue",25)
-jump(bob,125,-150)
-box(bob,50,"blue",25)
-jump(bob,150,-125)
-box(bob,50,"blue",25)
-jump(bob,175,-100)
-box(bob,50,"blue",25)
-jump(bob,200,-75)
-box(bob,50,"blue",25)
-jump(bob,225,-50)
-box(bob,50,"blue",25)
-jump(bob,250,-25)
-box(bob,50,"blue",25)
-jump(bob,225,0)
-box(bob,50,"blue",25)
-jump(bob,200,25)
-box(bob,50,"blue",25)
-jump(bob,175,50)
-box(bob,50,"blue",25)
-jump(bob,150,75)
-box(bob,50,"blue",25)
-jump(bob,125,100)
-box(bob,50,"blue",25)
+t.setposition(38,32) #now to create the yellow parts of the Superman logo
+t.pendown() #the pen is now poised to draw
+t.begin_fill() #start a new shape
+t.fillcolor('gold') #change the fill color to gold to match the Superman logo
+t.forward(13) #move the pen forward by 13
+t.right(120) #rotate the pen right by 120 degrees
+t.forward(13) #move the pen forward by 13
+t.right(120) #rotate the pen right by 120 degrees
+t.forward(13) #move the pen forward by 13
+t.end_fill() #the small triangle on the right is now complete
+t.penup() #stop the pen from drawing
 
-#left part
-jump(bob,-25,-275)
-box(bob,75,"blue",25)
-jump(bob,-50,-250)
-box(bob,50,"blue",25)
-jump(bob,-75,-225)
-box(bob,50,"blue",25)
-jump(bob,-100,-200)
-box(bob,50,"blue",25)
-jump(bob,-125,-175)
-box(bob,50,"blue",25)
-jump(bob,-150,-150)
-box(bob,50,"blue",25)
-jump(bob,-175,-125)
-box(bob,50,"blue",25)
-jump(bob,-200,-100)
-box(bob,50,"blue",25)
-jump(bob,-225,-75)
-box(bob,50,"blue",25)
-jump(bob,-250,-50)
-box(bob,50,"blue",25)
-jump(bob,-275,-25)
-box(bob,50,"blue",25)
-jump(bob,-250,0)
-box(bob,50,"blue",25)
-jump(bob,-225,25)
-box(bob,50,"blue",25)
-jump(bob,-200,50)
-box(bob,50,"blue",25)
-jump(bob,-175,75)
-box(bob,50,"blue",25)
-jump(bob,-150,100)
-box(bob,50,"blue",25)
+t.setposition(81.5,25) #now to create the larger yellow part of the Superman logo, change the position of the pen
+t.pendown() #the pen will now draw again
+t.begin_fill() #the fill is still the same color set before
+t.right(210) #rotate the pen right by 210 degrees
+t.forward(25) #move the pen forward by 25
+t.right(90) #rotate the pen right by 90 degrees
+t.forward(38) #move the pen forward by 38
+t.right(45) #rotate the pen right by 45 degrees
+t.circle(82,90) #this function is used to draw a circle in turtle, the first integer is the radius and the second is the number of degrees of the circle drawn
+t.left(90) #rotate the pen left by 90 degrees
+t.circle(82,60) #create a circle of radius 82 and only complete 60 degrees of the circle
+curve(61) #call the curve function that was previously defined, pass an integer value equal to the length of the curve desired
+t.left(90) #rotate the pen left by 90 degrees
+t.forward(57) #move the pen forward by 57
+t.left(90) #rotate the pen left by 90 degrees
+t.forward(32) #move the pen forward by 32
+t.end_fill() #fill in the larger yellow part of the logo
+t.penup() #stop drawing
+t.home() #reset the pen location to the origin so the orientation is also reset
 
-#extra red part
-jump(bob,0,-250)
-box(bob,25,"red",25)
-jump(bob,-25,-225)
-box(bob,75,"red",25)
+t.setposition(-69,-38) #finish the major parts of the S superimposition on the shield
+t.pendown()
+t.begin_fill()
+curve(20)
+t.forward(33)
+t.left(10)
+t.circle(82,20)
+curve(30)
+t.forward(10)
+t.right(110)
+curve(40)
+t.right(10)
+t.circle(50,10)
+curve(45)
+t.right(5)
+t.forward(45)
+t.end_fill()
+t.penup()
+t.home()
 
-#left red part
-jump(bob,-50,-200)
-box(bob,50,"red",25)
-jump(bob,-75,-175)
-box(bob,50,"red",25)
-jump(bob,-100,-150)
-box(bob,50,"red",25)
-jump(bob,-125,-125)
-box(bob,50,"red",25)
-jump(bob,-150,-100)
-box(bob,50,"red",25)
-jump(bob,-175,-75)
-box(bob,50,"red",25)
-jump(bob,-200,-50)
-box(bob,50,"red",25)
-jump(bob,-225,-25)
-box(bob,50,"red",25)
-jump(bob,-200,0)
-box(bob,50,"red",25)
-jump(bob,-175,25)
-box(bob,50,"red",25)
-jump(bob,-150,50)
-box(bob,50,"red",25)
+t.setposition(20,-100)
+t.pendown()
+t.begin_fill()
+t.right(135)
+t.forward(27)
+t.right(90)
+t.forward(27)
+t.right(135)
+t.forward(38.18)
+t.end_fill()
+t.penup()
+t.home()
 
-#red right part
-jump(bob,25,-200)
-box(bob,50,"red",25)
-jump(bob,50,-175)
-box(bob,50,"red",25)
-jump(bob,75,-150)
-box(bob,50,"red",25)
-jump(bob,100,-125)
-box(bob,50,"red",25)
-jump(bob,125,-100)
-box(bob,50,"red",25)
-jump(bob,150,-75)
-box(bob,50,"red",25)
-jump(bob,175,-50)
-box(bob,50,"red",25)
-jump(bob,200,-25)
-box(bob,50,"red",25)
-jump(bob,175,0)
-box(bob,50,"red",25)
-jump(bob,150,25)
-box(bob,50,"red",25)
-jump(bob,125,50)
-box(bob,50,"red",25)
+t.setposition(-57,32)
+t.pendown()
+t.begin_fill()
+t.right(180)
+t.forward(18)
+t.left(45)
+t.forward(44)
+t.left(80)
+t.forward(15)
+t.left(130)
+curve(40)
+t.forward(20)
+t.end_fill()
 
-#inside part
-jump(bob,0,-200)
-box(bob,25,"yellow",25)
-jump(bob,-25,-175)
-box(bob,75,"yellow",25)
-jump(bob,-50,-150)
-box(bob,125,"red",25)
-jump(bob,-75,-125)
-box(bob,25,"yellow",25)
-jump(bob,-50,-125)
-box(bob,50,"red",25)
-jump(bob,0,-125)
-box(bob,75,"yellow",25)
-jump(bob,75,-125)
-box(bob,25,"red",25)
-jump(bob,-100,-100)
-box(bob,200,"yellow",25)
-jump(bob,100,-100)
-box(bob,25,"red",25)
-jump(bob,-125,-75)
-box(bob,275,"red",25)
-jump(bob,-150,-50)
-box(bob,25,"yellow",25)
-jump(bob,-125,-50)
-box(bob,250,"red",25)
-jump(bob,125,-50)
-box(bob,50,"yellow",25)
-jump(bob,-175,-25)
-box(bob,50,"yellow",25)
-jump(bob,-125,-25)
-box(bob,50,"red",25)
-jump(bob,-75,-25)
-box(bob,275,"yellow",25)
-jump(bob,-150,0)
-box(bob,25,"yellow",25)
-jump(bob,-125,0)
-box(bob,50,"red",25)
-jump(bob,-75,0)
-box(bob,150,"yellow",25)
-jump(bob,75,0)
-box(bob,75,"red",25)
-jump(bob,150,0)
-box(bob,25,"yellow",25)
-jump(bob,-125,25)
-box(bob,25,"yellow",25)
-jump(bob,-100,25)
-box(bob,50,"red",25)
-jump(bob,-50,25)
-box(bob,100,"yellow",25)
-jump(bob,50,25)
-box(bob,50,"red",25)
-jump(bob,100,25)
-box(bob,25,"yellow",25)
-jump(bob,125,25)
-box(bob,25,"red",25)
-jump(bob,-100,50)
-box(bob,225,"red",25)
+t.hideturtle()
+turtle.exitonclick()
